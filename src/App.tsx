f2ac04865ae0d283efd527edff51ef8217f72b4d
Oscar Arbelaez <oscar@arbelaez.dev>
2024-03-01 23:37:11 +0000

    Add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button";
 import viteLogo from "/vite.svg";
 import { useState } from "react";
 
@@ -13,9 +14,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
