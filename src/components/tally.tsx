ebe5b4c5a9d4702d62cf6df7c14b86463908deaf
Oscar Arbelaez <oscar@arbelaez.dev>
2024-03-10 17:56:09 +0000

    Add favicon and improve tally

--- src/components/tally.tsx
+++ src/components/tally.tsx
@@ -10,43 +10,49 @@ export const Tally = ({ story }: TallyProps) => {
   const allVoted = Object.keys(story.participants || {}).every(
     (uid) => story.votes && !!story.votes[uid],
   );
+  const participants = Object.entries(story.participants || {});
+  const observers = Object.entries(story.observers || {});
   if (!user || user === "loading") {
     return null;
   }
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-bold">Participants</h2>
-      <ul className="flex flex-col gap-2">
-        {Object.entries(story.participants || {}).map(([uid, { name }]) => (
-          <li
-            key={uid}
-            className="flex gap-4 items-center justify-between max-w-[24ch]"
-          >
-            <div className="flex items-center gap-1">
-              {story.votes && !!story.votes[uid] && <RxCheck />}
-              <span
-                title={name}
-                className="whitespace-nowrap overflow-hidden overflow-ellipsis"
+      {participants.length > 0 && (
+        <>
+          <h2 className="text-2xl font-bold">Participants</h2>
+          <ul className="flex flex-col gap-2">
+            {participants.map(([uid, { name }]) => (
+              <li
+                key={uid}
+                className="flex gap-4 items-center justify-between max-w-[24ch]"
               >
-                {name}
-              </span>
-            </div>
-            {allVoted || story.votes && story.votes[uid] && uid === user.uid ? (
-              <span>{story.votes && story.votes[uid]}</span>
-            ) : (
-              <div
-                className="flex-shrink-0 bg-foreground w-8 h-4 rounded-md"
-                aria-label="hidden vote"
-              />
-            )}
-          </li>
-        ))}
-      </ul>
-      {Object.entries(story.observers || {}).length > 0 && (
+                <div className="flex items-center gap-1">
+                  {story.votes && !!story.votes[uid] && <RxCheck />}
+                  <span
+                    title={name}
+                    className="whitespace-nowrap overflow-hidden overflow-ellipsis"
+                  >
+                    {name}
+                  </span>
+                </div>
+                {allVoted || story.votes && story.votes[uid] && uid === user.uid ? (
+                  <span>{story.votes && story.votes[uid]}</span>
+                ) : (
+                  <div
+                    className="flex-shrink-0 bg-foreground w-8 h-4 rounded-md"
+                    aria-label="hidden vote"
+                  />
+                )}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      {observers.length > 0 && (
         <>
           <h2 className="text-2xl font-bold">Observers</h2>
           <ul className="flex flex-col gap-2">
-            {Object.entries(story.observers || {}).map(([uid, { name }]) => (
+            {observers.map(([uid, { name }]) => (
               <li
                 key={uid}
                 className="flex gap-4 items-center justify-between max-w-[24ch]"
